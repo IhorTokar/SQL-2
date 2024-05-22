567a32e8af582006db0252d2c80916ff9adcdf67
Ihor Tokar <134220707+IhorTokar@users.noreply.github.com>
2024-05-22 23:48:09 +0300

    Update and rename SQLQuery1.sql to Доповнення1.sql

--- Доповнення1.sql
+++ Доповнення1.sql
@@ -1,20 +1,20 @@
 USE CulinaryForum;
--- Вставка даних у таблицю Roles
+-- Р’СЃС‚Р°РІРєР° РґР°РЅРёС… Сѓ С‚Р°Р±Р»РёС†СЋ Roles
 INSERT INTO Roles (role_name) VALUES ('Admin'), ('User'), ('Guest');
 GO
 
--- Приклад додавання користувача
+-- РџСЂРёРєР»Р°Рґ РґРѕРґР°РІР°РЅРЅСЏ РєРѕСЂРёСЃС‚СѓРІР°С‡Р°
 INSERT INTO Users (username, email, password_hash, role_id)
 VALUES ('newuser', 'newuser@example.com', 'hashed_password', 2);
 GO
 
--- Приклад оновлення користувача
+-- РџСЂРёРєР»Р°Рґ РѕРЅРѕРІР»РµРЅРЅСЏ РєРѕСЂРёСЃС‚СѓРІР°С‡Р°
 UPDATE Users
 SET email = 'updateduser@example.com'
 WHERE username = 'newuser';
 GO
 
--- Перевірка користувачів і їхніх ролей
+-- РџРµСЂРµРІС–СЂРєР° РєРѕСЂРёСЃС‚СѓРІР°С‡С–РІ С– С—С…РЅС–С… СЂРѕР»РµР№
 SELECT user_id, username, email, created_at, updated_at
 FROM Users;
 GO
